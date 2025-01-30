dac1ea2f7653bad6c18746af69589099458e3c2a
Juho Simojoki <juho.simojoki@outlook.com>
2025-01-30 15:05:43 +0200

    Font added and some texr, upper banner has been resized

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
         <img className="size-14" src="./src/assets/icons8-logo.svg"/>
         <h1>LOGO</h1>
       </div>
-      <ul className='flex flex-row gap-2 uppercase'>
+      <ul className='flex flex-row gap-2 uppercase text-sm'>
         <li><a href="#home">home</a></li>
         <li><a href="#info">info</a></li>
         <li><a href="#services">services</a></li>
--- src/components/UpperBanner.tsx
+++ src/components/UpperBanner.tsx
@@ -1,9 +1,10 @@
 const UpperBanner = () => {
   return (
-    <div id="home" className="w-full min-h-72
+    <div id="home" className="w-full min-h-screen 
       bg-[url('./assets/water-drop.jpg')] bg-no-repeat bg-cover bg-center bg-fixed
-      flex items-center justify-center">
-      <p>CONTENTTIA</p>
+      flex flex-col items-center justify-center gap-5 text-center">
+        <h2 id="banner-heading" className="font-banner-heading italic text-8xl text-[#efdf75]">Moi!</h2>
+        <p id="banner-intro" className="xl:w-2xl text-2xl">Tähän tulee lyhyt <strong>esittely</strong>, joka luo asiakkaalle ensimmäisen mielikuvan sinusta graafisena suunnittelijana.</p>
     </div>
   )
 }
