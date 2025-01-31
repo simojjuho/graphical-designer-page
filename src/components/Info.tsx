6d2ee4f1b3dc5352596b60c57455134d7a38ecf4
Juho Simojoki <juho.simojoki@outlook.com>
2025-01-31 16:47:14 +0200

    Editin the info section

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,7 +1,16 @@
 const Info = () => {
   return (
-    <div id='info' className='h-72 bg-[#efdf75]'>
-      CONTENTTIA
+    <div id='info' className='h-fit p-16 bg-[#efdf75]'>
+      <h2
+        className="mb-12 text-4xl uppercase font-bold font-info text-center"
+      >info</h2>
+      <div className="leading-9 text-center flex lg:flex-row flex-col gap-10">
+        <p className="text-2xl lg:text-right grow-2 ">Sed at ante in diam pharetra commodo. Fusce vulputate, orci eu viverra molestie, dui nunc convallis enim, ac vehicula nunc leo id elit.</p>
+        <p className="lg:text-left grow-1">Integer venenatis dui arcu, vulputate maximus libero tincidunt nec. Donec elementum efficitur bibendum. Fusce diam ante, venenatis sit amet neque ac, cursus eleifend justo. Nam faucibus scelerisque dolor vel maximus. Mauris molestie aliquam eros, nec molestie urna laoreet nec. Aliquam sapien nunc, vehicula sit amet accumsan bibendum, molestie eu sem. Nulla ultrices neque vel scelerisque faucibus.</p>
+        <p className="lg:text-left grow-1">Vestibulum pulvinar accumsan libero sit amet cursus. Phasellus aliquam in sapien ut ullamcorper. Etiam hendrerit id turpis sed interdum. Proin feugiat purus non neque consectetur congue. Nullam dapibus iaculis leo. Mauris blandit tellus ligula. In nec velit sapien. Maecenas mi purus, pretium non augue nec, cursus pretium diam. Duis tempus pulvinar rutrum. Nullam vestibulum tortor sed tellus ullamcorper ultrices.
+
+</p>
+      </div>
     </div>
   )
 }
